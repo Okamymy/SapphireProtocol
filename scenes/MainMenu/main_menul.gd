00ce4bc2252914bf6dbf4d005ffe03b5bce94e4a
lladux <lladux@protonmail.com>
2025-07-27 21:28:59 -0700

    fix main menu

--- scenes/MainMenu/main_menul.gd
+++ scenes/MainMenu/main_menul.gd
@@ -1,25 +1,26 @@
 extends Control
 
-@onready var start: Button = $start
-@onready var exit: Button = $exit
-@onready var options: Button = $options
-@onready var achievements: Button = $achievements
-
-var siguiente_escena = preload("res://scenes/TD/word01.tscn")
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(siguiente_escena)
+@onready var panel_settings: Panel = $PanelSettings
+@onready var menu_main: VBoxContainer = $MenuMain
 
 
+var siguiente_escena = preload("res://scenes/TD/levels/word01.tscn")
+
+# Main menu
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_options_pressed() -> void:
-	print('ola')
- # Replace with function body.
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_packed(siguiente_escena)
 
 
-func _on_achievements_pressed() -> void:
-	print('ola')
- # Replace with function body.
+func _on_settings_pressed() -> void:
+	menu_main.visible=false
+	panel_settings.visible=true
+	
+
+# Settings
+func _on_back_settings_button_up() -> void:
+	menu_main.visible=true
+	panel_settings.visible=false
